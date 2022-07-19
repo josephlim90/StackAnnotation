5654427e3f8834c730c1c41239a710f7467f623d
josephlim90 <josephlim90@gmail.com>
2022-07-19 11:12:21 +0800

    fail the test case 2

--- src/stack/MyStringStackTest.java
+++ src/stack/MyStringStackTest.java
@@ -3,7 +3,7 @@ package stack;
 public class MyStringStackTest {
 	public static void main(String[] args){
 		MyStringStack stack = new MyStringStack();
-		stack.push (s1);
+		stack.push ("s1");
 		assertTrue (stack.isEmpty());
 	}
 }
