bb3f554dd2085a29deccc9d4001b6eb03cea857f
josephlim90 <josephlim90@gmail.com>
2022-07-19 11:08:49 +0800

    fail the test case

---
+++ src/stack/MyStringStackTest.java
@@ -0,0 +1,9 @@
+package stack;
+
+public class MyStringStackTest {
+	public static void main(String[] args){
+		MyStringStack stack = new MyStringStack();
+		stack.push (s1);
+		assertTrue (stack.isEmpty());
+	}
+}
